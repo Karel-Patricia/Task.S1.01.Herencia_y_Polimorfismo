3e1cc2eb3aa84db30d3f72cfc16bba0e36243ee6
Patricia Paredes <patriciakarel@hotmail.com>
2025-12-16 19:17:55 +0100

    test: demonstrate static and final behavior using console output

--- Nivel1/Ejercicio2/src/main/Main.java
+++ Nivel1/Ejercicio2/src/main/Main.java
@@ -5,9 +5,28 @@ import ejercicio2nivel1.Car;
 public class Main {
     static void main(String[] args) {
 
-        Car.brake();
+        //Muestra valores iniciales
+        System.out.println("brand : " + Car.brand);
+        System.out.println("model : " + Car.model);
 
-        Car car = new Car(150);
-        car.accelerate();
+        //Dos coches con distinta potencia
+        Car car1 = new Car(100);
+        Car car2 = new Car(140);
+
+        System.out.println("Potencia car1: " + car1.power);
+        System.out.println("Potencia car2: " + car2.power);
+
+        //Cambio de modelo
+        Car.model = "Princes";
+
+        System.out.println("Modelo después del cambio:");
+        System.out.println("Modelo car1: " + Car.model);
+        System.out.println("Modelo car2: " + Car.model);
+
+
+        Car.brake();
+        car1.accelerate();
+        car2.accelerate();
+
     }
 }
